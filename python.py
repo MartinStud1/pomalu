f8b1695c8e6f525e8fd3f5bc006ecf4f2ac97772
MartinStud1 <156411654+MartinStud1@users.noreply.github.com>
2024-06-02 10:02:47 +0000

    Uživatelé v statistikách + statistiky - jmeno+prijmeni + znovu přicházení na nějaké problémy - asi x10. Funguje.

--- python.py
+++ python.py
@@ -5,6 +5,11 @@ uzivatele = []
 statistika = {    #list pro statistiku
     "senior_s" : 0, "vydej_s" : 0, "beskydy_s" : 0, "komunitzah_s" : 0, "promale_s" : 0
 }
+uzivatele1 = []
+uzivatele2 = []
+uzivatele3 = []
+uzivatele4 = []
+uzivatele5 = []
 
 #p_x = přihlašovací ...
 app.secret_key = "nikdonicnetusit"
@@ -65,8 +70,7 @@ def komunitzah():
 
 
 
-hlavni_jmeno = None
-hlavni_prijmeni = None
+
 @app.route("/prihlas_se", methods=["GET","POST"])
 def prihlas_se():
     jmeno = session.get("jmeno")
@@ -84,21 +88,16 @@ def prihlas_se():
     p_email = request.form.get("p_email")
     p_telefon = request.form.get("p_telefon")
     p_heslo = request.form.get("p_heslo")
-
-
     p_pole = [p_jmeno, p_prijmeni, p_email, p_telefon, p_heslo] #poznámka - není třeba dělat pole v poli u přihlašování, protože tam nebude více než jedno zadané tím člověkem... stačí jedno pole
 
     spravne = 0
     mam_te = 0
     if request.method == "POST":
-        for i in range(len(uzivatele)):
-            
+        for i in range(len(uzivatele)):  
             for n in range(len(p_pole)):
-                 
-                                                                    
+                                                                      
                 if uzivatele[i][n] == p_pole[n]:
-                    spravne += 1
-                       
+                    spravne += 1          
             if spravne == len(p_pole):
                 mam_te = 1
                 
@@ -109,58 +108,75 @@ def prihlas_se():
                 
         if mam_te == 1:
             porovnani = "Jsi přihlášen."
-            hlavni_jmeno = p_pole[0]
-            hlavni_prijmeni = p_pole[1]
+            hl_jmeno = request.form.get("p_jmeno")
+            hl_prijmeni = request.form.get("p_prijmeni")
+            session['hl_jmeno'] = hl_jmeno #uložím do relace
+            session['hl_prijmeni'] = hl_prijmeni #uložím do relace
+            
         if mam_te != 1:
             porovnani = "něco nám nesedí" 
-            hlavni_jmeno = None
-            hlavni_prijmeni = None
+            
     
     return render_template("prihlas_se.html",spravne=porovnani) 
  
+
+
 @app.route("/senior_s", methods=["GET", "POST"])
 def senior_s():
-    
     if request.method == "POST":
-        
         statistika["senior_s"] += 1
+        hl_jmeno = session.get("hl_jmeno")
+        hl_prijmeni = session.get("hl_prijmeni")
+        uzivatele1.append(hl_jmeno)
+        uzivatele1.append(hl_prijmeni)
     return render_template("senior.html")
 
 @app.route("/beskydy_s", methods=["GET", "POST"])
 def beskydy_s():
     if request.method == "POST":
-        
         statistika["beskydy_s"] += 1
+        hl_jmeno = session.get("hl_jmeno")
+        hl_prijmeni = session.get("hl_prijmeni")
+        uzivatele2.append(hl_jmeno)
+        uzivatele2.append(hl_prijmeni)
+        
     return render_template("beskydy.html")
 
 @app.route("/promale_s", methods=["GET", "POST"])
 def promale_s():
     if request.method == "POST":
-        
         statistika["promale_s"] += 1
+        hl_jmeno = session.get("hl_jmeno")
+        hl_prijmeni = session.get("hl_prijmeni")
+        uzivatele3.append(hl_jmeno)
+        uzivatele3.append(hl_prijmeni)
     return render_template("promale.html")
 
 @app.route("/komunitzah_s", methods=["GET", "POST"])
 def komunitzah_s():
     if request.method == "POST":
-        
         statistika["komunitzah_s"] += 1
+        hl_jmeno = session.get("hl_jmeno")
+        hl_prijmeni = session.get("hl_prijmeni")
+        uzivatele4.append(hl_jmeno)
+        uzivatele4.append(hl_prijmeni)
     return render_template("komunitzah.html")
 
 @app.route("/vydej_s", methods=["GET", "POST"])
 def vydej_s():
     if request.method == "POST":
-       
         statistika["vydej_s"] += 1
+        hl_jmeno = session.get("hl_jmeno")
+        hl_prijmeni = session.get("hl_prijmeni")
+        uzivatele5.append(hl_jmeno)
+        uzivatele5.append(hl_prijmeni)
     return render_template("vydej.html")
 
 @app.route("/statistika", methods = ["GET", "POST"])
 def statistikafunkce():
     
-    return render_template("statistika.html", statistika=statistika)
+    return render_template("statistika.html", statistika=statistika,jmeno1 = uzivatele1, jmeno2 = uzivatele2, jmeno3 = uzivatele3, jmeno4 = uzivatele4, jmeno5 = uzivatele5)
     
-
-
 if __name__ == "__main__":
     app.run(debug=True)
 
